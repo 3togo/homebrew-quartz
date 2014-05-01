72bece1319b3e9656c14f375d4edea5d40f89a50
FreeToGo <freetogo@gmail.com>
2014-05-01 11:52:33 +0800

    Add GTK2 supprt

--- geany-gtk2-quartz.rb
+++ geany-gtk2-quartz.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class GeanyQuartz < Formula
+class GeanyGtk2Quartz < Formula
   homepage 'http://geany.org/'
   url 'http://download.geany.org/geany-1.24.1.tar.gz'
   sha1 '2707b6bbcc4710e3dca990d26f66d679d82a2cc0'
@@ -9,14 +9,15 @@ class GeanyQuartz < Formula
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
   depends_on 'gettext'
-  depends_on '3togo/quartz/gtk+-quartz'
+  depends_on '3togo/quartz/gtk2+-quartz'
+  #depends_on "gtk2+-quartz"
   depends_on 'hicolor-icon-theme'
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
-                          "--enable-gtk3",
+                          #"--enable-gtk3",
                           #"--disable-debug",
                           "--enable-debug",
                           "--enable-introspection=yes"
---
+++ gtk2+-quartz.rb
@@ -0,0 +1,46 @@
+require 'formula'
+
+class Gtk2xQuartz < Formula
+  homepage 'http://www.gtk.org/'
+  url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/2.24/gtk+-2.24.23.tar.xz'
+  sha256 'a0a406e27e9b5e7d6b2c4334212706ed5cdcd41e713e66c9ae950655dd61517c'
+ # url 'http://ftp.gnome.org/pub/GNOME/sources/gtk+/3.12/gtk+-3.12.1.tar.xz'
+ # sha256 '719aae5fdb560f64cadb7e968c8c85c0823664de890c9f765ff4c0efeb0277cd'
+  depends_on 'pkg-config' => :build
+  depends_on 'glib'
+  depends_on 'jpeg'
+  depends_on 'libtiff'
+  depends_on 'gdk-pixbuf'
+  depends_on 'jasper' => :optional
+  depends_on 'atk'
+
+  depends_on '3togo/quartz/pango-quartz'
+  depends_on '3togo/quartz/cairo-quartz'
+  keg_only 'This formula builds Gtk+ for use with Quartz instead of X11, which is experimental.'
+
+  fails_with_llvm 'Undefined symbols when linking', :build => '2326' unless MacOS.lion?
+
+  def install
+    ENV.append 'LDFLAGS', '-framework Carbon -framework Cocoa'
+    # gtk-update-icon-cache is used during installation, and
+    # we don't want to add a dependency on gtk+2 just for this.
+    #inreplace %w[ gtk/makefile.msc.in
+                  #demos/gtk-demo/Makefile.in
+                  #demos/widget-factory/Makefile.in ],
+                  #/gtk-update-icon-cache --(force|ignore-theme-index)/,
+                  #"#{buildpath}/gtk/\\0"
+
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-debug', '--disable-dependency-tracking',
+                          '--disable-glibtest', 
+                          '--with-gdktarget=quartz', 
+                          '--disable-introspection',
+                          '--enable-quartz-backend'
+
+    system 'make install'
+  end
+
+  #def test
+  #  system '#{bin}/gtk-demo'
+  #end
+end
